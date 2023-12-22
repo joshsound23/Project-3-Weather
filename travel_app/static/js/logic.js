ea834ffbd549683c0bc61fb1591f434ea62e78bb
matthill <matthewhill60625@gmail.com>
2023-12-21 20:28:18 -0600

    starter js code

---
+++ travel_app/static/js/logic.js
@@ -0,0 +1,21 @@
+
+// Load Json based on API route
+
+
+// Create Map Object
+let myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 3
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+// Add popup Markers
+
+// Select marker function to update charts
+
+// Create 3 charts - Temp, Precip, Wind
