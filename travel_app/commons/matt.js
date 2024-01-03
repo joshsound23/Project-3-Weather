b62dc34c8fe021593373502cf345ffe9aa516ada
matthill4459 <140844143+matthill4459@users.noreply.github.com>
2024-01-02 18:41:29 -0600

    backup

--- travel_app/commons/matt.js
+++ travel_app/commons/matt.js
@@ -1,16 +1,199 @@
 console.log()
+
+  
 // Create Map Object
 let myMap = L.map("map", {
-    center: [51.4999947297, -0.11672184386],
-    zoom: 12
+    center: [45.52, -122.67],
+    zoom: 3
   });
   
   // Adding a tile layer (the background map image) to our map:
-  // We use the addTo() method to add objects to our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
- 
+
+// Setting a static JSON for testing
+  
+
+// Add popup Markers
+for (let i = 0; i < city_data.length; i++) {
+  let city = city_data[i];
+  L.marker([city.latitude, city.longitude])
+    .bindPopup(`${city.city_name}`)
+    .addTo(myMap).on('click', onClick);
+}
+
+// Select marker function to update charts
+
+// Click and log the city name
+function onClick(e) {
+  var popup = e.target.getPopup();
+  var content = popup.getContent();
+
+  console.log(content);
+
+  // Filter JSON by city clicked on
+  filtered = city_data.filter(function (i) {
+    return i.city_name === content;
+  });
+    console.log(filtered);
+
+    // Create list of temperatures in that city for the time frame
+    const temps = [];
+    for (let i = 0; i < filtered.length; i++) {
+      let temp_record = filtered[i];
+      temps.push(temp_record.avg_temp_c);
+
+    }
+    console.log(temps);
+
+    // Average list of temperatures to get an average temperature for time frame
+    var total = 0;
+for(var i = 0; i < temps.length; i++) {
+    total += temps[i];
+}
+var avg_temp = total / temps.length;
+
+console.log(avg_temp);
+
+}
+  
+
+// Create 3 charts - Temp, Precip, Wind
+
+// Temperature Chart https://jsfiddle.net/fusioncharts/ND2WL/
+FusionCharts.ready(function () {
+  var chart = new FusionCharts({
+      type: 'thermometer',
+      renderAt: 'temp',
+      id  : 'cityTemp',
+      width: '240',
+      height: '300',
+      dataFormat: 'json',
+      dataSource: {
+          "chart": {
+              "caption": "Avg. Temperature",
+              "subcaption": " City Name",
+              "lowerLimit": "-20",
+              "upperLimit": "20",
+              "numberSuffix": "°C",
+              "showhovereffect": "1",
+              "thmFillColor": "#008ee4",
+              "showGaugeBorder" : "1",
+              "gaugeBorderColor" :  "#008ee4",
+              "gaugeBorderThickness" :  "2",
+              "gaugeBorderAlpha" :  "30",
+              "thmOriginX": "100",
+              "theme" : "fint"
+          },
+          "value": "-6",
+          //All annotations are grouped under this element
+          "annotations": {
+              "showbelow": "0",
+              "groups": [                        
+                  {                  
+                      //Each group needs a unique ID
+                      "id": "indicator",
+                      "items": [
+                          //Showing Annotation
+                          {
+                              "id": "background",
+                              //Polygon item 
+                              "type": "rectangle",
+                              "alpha" : "50",
+                              "fillColor": "#AABBCC",           
+                              "x" : "$gaugeEndX-35",
+                               "tox" : "$gaugeEndX",
+                              "y" : "$gaugeEndY+55",
+                              "toy" : "$gaugeEndY+72"
+                          }
+                      ]
+                  }
+              ]
+              
+          },
+      },
+      "events" :{
+          "rendered" : function (evt, arg) {
+              var chargeInterval = setInterval( function(){
+                  var temp = parseInt(Math.random()*2) -5;
+                  FusionCharts.items["cityTemp"].feedData("&value="+temp);
+              }, 4000);
+          }   
+      }
+  })
+  .render();
+});
+
+// Rainfall Chart https://jsfiddle.net/fusioncharts/MWnVr/
+FusionCharts.ready(function() {
+  var rainVolume = 110,
+    fuelWidget = new FusionCharts({
+      type: 'cylinder',
+      dataFormat: 'json',
+      id: 'cityPrecip',
+      renderAt: 'precip',
+      width: '240',
+      height: '300',
+      dataSource: {
+        "chart": {
+          "theme": "fusion",
+          "caption": "Avg. Precipitation",
+          "subcaption": "Rain",
+          "lowerLimit": "0",
+          "upperLimit": "120",
+          "lowerLimitDisplay": "Empty",
+          "upperLimitDisplay": "Full",
+          "numberSuffix": " inches",
+          "showValue": "1",
+          "chartBottomMargin": "45",
+          "showValue": "0"
+        },
+        "value": "75",
+        "annotations": {
+          "origw": "400",
+          "origh": "190",
+          "autoscale": "1",
+          "groups": [{
+            "id": "range",
+            "items": [{
+                "id": "rangeBg",
+                "type": "rectangle",
+                "x": "$canvasCenterX-45",
+                "y": "$chartEndY-30",
+                "tox": "$canvasCenterX +45",
+                "toy": "$chartEndY-75",
+                "fillcolor": "#6caa03"
+              },
+              {
+                "id": "rangeText",
+                "type": "Text",
+                "fontSize": "11",
+                "fillcolor": "#333333",
+                "text": "80 ltrs",
+                "x": "$chartCenterX-45",
+                "y": "$chartEndY-50"
+              }
+            ]
+          }]
+        }
+
+      },
+      "events": {
+        "rendered": function(evtObj, argObj) {
+          setInterval(function() {
+            (rainVolume < 10) ? (rainVolume = 80) : "";
+            var consVolume = rainVolume - (Math.floor(Math.random() * 3));
+            
+          }, 1000);
+        },
+        
+      }
+    }).render();
+});
+
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
   let weather_data = '../prework_folder/test.json';
   let city_data = '../prework_folder/cities_data.json'
   
