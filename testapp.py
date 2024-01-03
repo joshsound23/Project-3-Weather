6bb2da68296c997564de73075096347eef085e7d
joshsound23 <joshsound23@gmail.com>
2024-01-02 18:40:21 -0600

    Updated testapp.py and html

--- testapp.py
+++ testapp.py
@@ -14,15 +14,25 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['project3_weather']  # Replace 'your_database_name' with your database name
 collection = db['merged_data']  # Replace 'your_collection_name' with your collection name
 
+def get_random_cities(query, city_limit):
+    pipeline = [
+        {'$match': query},
+        {'$sample': {'size': city_limit}},
+        {'$unset': '_id'}
+    ]
+    result = list(collection.aggregate(pipeline))
+    return jsonify(result)
+
 @app.route('/us-cities', methods=['GET'])
 def us_cities():
     # Retrieve user inputs for temperature and season
     temperature = request.args.get('temperature')
     season = request.args.get('season')
+    city_limit = 5
 
-    # Prepare the query for U.S. cities based on user inputs
+    # Prepare the query for U.S. cities based on user inputs 
     query = {
-        'country': 'Moldova'
+        'country': 'United States of America'
     }
 
     # Add optional filters based on user input for temperature and season
@@ -30,20 +40,22 @@ def us_cities():
         query['avg_temp_c'] = float(temperature)
     if season:
         query['season'] = season
+    return get_random_cities(query, city_limit)
 
     # Perform MongoDB query for U.S. cities, excluding the _id field
-    cursor = collection.find(query, {'_id': 0})
+    #cursor = collection.find(query, {'_id': 0})
 
     # Convert MongoDB cursor to a JSON-serializable list of dictionaries using bson.json_util
-    us_cities_filtered = json_util.dumps(list(cursor))
+    #us_cities_filtered = json_util.dumps(list(cursor))
 
-    return us_cities_filtered
+    #return us_cities_filtered
 
 @app.route('/international-cities', methods=['GET'])
 def international_cities():
     # Retrieve user inputs for temperature and season
     temperature = request.args.get('temperature')
     season = request.args.get('season')
+    city_limit = 5
 
     # Prepare the query for international cities based on user inputs
     query = {
@@ -55,6 +67,7 @@ def international_cities():
         query['avg_temp_c'] = float(temperature)
     if season:
         query['season'] = season
+    return get_random_cities(query, city_limit)
 
     # Perform MongoDB query for international cities, excluding the _id field
     cursor = collection.find(query, {'_id': 0})
