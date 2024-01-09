943c0e7ba21fad442058b70ce90fb8957a36da5e
Ben Belczak <benbelczak33@gmail.com>
2024-01-08 21:05:03 -0600

    new files from other branches

---
+++ testapp.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from bson import json_util
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('indextwo.html')
+
+# Connect to your MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Project3_Weather']  # Replace 'your_database_name' with your database name
+collection = db['weather_data']  # Replace 'your_collection_name' with your collection name
+
+def get_random_cities(query, city_limit):
+    pipeline = [
+        {'$match': query},
+        {'$sample': {'size': city_limit}},
+        {'$unset': '_id'}
+    ]
+    result = list(collection.aggregate(pipeline))
+    return jsonify(result)
+
+@app.route('/us-cities', methods=['GET'])
+def us_cities():
+    # Retrieve user inputs for temperature and season
+    temperature = request.args.get('temperature')
+    season = request.args.get('season')
+    city_limit = 5
+
+    # Prepare the query for U.S. cities based on user inputs 
+    #query = {
+    #    'country': 'United States of America'
+    #}
+    query = {
+        'country': {'$eq': 'United States of America'}
+    }
+
+    # Add optional filters based on user input for temperature and season
+    if temperature:
+        query['avg_temp_c'] = float(temperature)
+    if season:
+        query['season'] = season
+    data = get_random_cities(query, city_limit)
+    return get_random_cities(query, city_limit)
+    # Perform MongoDB query for U.S. cities, excluding the _id field
+    #cursor = collection.find(query, {'_id': 0})
+
+    # Convert MongoDB cursor to a JSON-serializable list of dictionaries using bson.json_util
+    #us_cities_filtered = json_util.dumps(list(cursor))
+
+    #return us_cities_filtered
+
+@app.route('/international-cities', methods=['GET'])
+def international_cities():
+    # Retrieve user inputs for temperature and season
+    temperature = request.args.get('temperature')
+    season = request.args.get('season')
+    city_limit = 5
+
+    # Prepare the query for international cities based on user inputs
+    query = {
+        'country': {'$ne': 'United States of America'}
+    }
+
+    # Add optional filters based on user input for temperature and season
+    if temperature:
+        query['avg_temp_c'] = float(temperature)
+    if season:
+        query['season'] = season
+    return get_random_cities(query, city_limit)
+
+    # Perform MongoDB query for international cities, excluding the _id field
+    #cursor = collection.find(query, {'_id': 0})
+
+    # Convert MongoDB cursor to a JSON-serializable list of dictionaries using bson.json_util
+    #international_cities_filtered = json_util.dumps(list(cursor))
+
+    #return international_cities_filtered
+
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
--- travel_app/static/js/logic.js
+++ travel_app/static/js/logic.js
@@ -1,26 +1,169 @@
-// Capture data
-const database = client.db('Project3_Weather').collection('weather_data');
-
-// Find documents in the collection
-collection.find({}).toArray((err, documents) => {
-    if (err) {
-      console.error('Error fetching documents:', err);
-      return;
-    }
-  
-    // Process the retrieved documents
-    console.log('Retrieved documents:', documents);
-  
-    // Close the connection after finishing operations
-    client.close();
-});
-
-// Get city, date, wind, and sunshine data
-
 // Find the average wind data by day per city
+const wind = [];
+for (let i = 0; i < filtered.length; i++) {
+  let wind_record = filtered[i];
+  wind.push(wind_record.avg_wind_speed_kmh);
+}
+console.log(wind);
+
+var total_wind = 0;
+
+for (var i = 0; i < wind.length; i++) {
+  total_wind += wind[i];
+}
+
+avg_wind = total_wind / wind.length;
+
+console.log(avg_wind);
 
 // Find the average sunshine data by day per city
+const sun = [];
+for (let i = 0; i < filtered.length; i++) {
+  let sun_record = filtered[i];
+  sun.push(sun_record.sunshine_total_min);
+}
+console.log(sun);
+
+var total_sun = 0;
+
+var total_sun = 0;
+for (let i = 0; i < sun.length; i++) {
+  total_sun += sun[i];
+}
+
+avg_sun = total_sun / sun.length;
+
+console.log(avg_sun);
 
 // Create wind chart
+FusionCharts.ready(function() {
+  var myChart = new FusionCharts({
+    type: "hlineargauge",
+    renderAt: "wind",
+    id: "cityWind",
+    width: "240",
+    height: "300",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Avg. Wind Speed",
+        subcaption: "",
+        theme: "gammel",
+        showvalue: "0",
+        pointerbghovercolor: "#ffffff",
+        pointerbghoveralpha: "80",
+        pointerhoverradius: "12",
+        showborderonhover: "1",
+        pointerborderhovercolor: "#333333",
+        pointerborderhoverthickness: "2",
+        showtickmarks: "0",
+        numbersuffix: "kmh"
+      },
+      colorrange: {
+        color: [
+          {
+            minvalue: "0",
+            maxvalue: "20",
+            label: "Calm",
+            code: "#32e3e3"
+          },
+          {
+            minvalue: "20",
+            maxvalue: "40",
+            label: "Breezy",
+            code: "#f9f9f9"
+          },
+          {
+            minvalue: "40",
+            maxvalue: "100",
+            label: "Windy",
+            code: "#888e8e"
+          }
+        ]
+      },
+      pointers: {
+        pointer: [
+          {
+            value: avg_wind,
+            tooltext: "$datavalue kilometers per hour of wind"
+          }
+        ]
+      }
+    };
+    "events" :{
+      "rendered" : function (evnt, argt) {
+          var chargeInterval = setInterval( function(){
+              var wind = avg_wind;
+              FusionCharts.items["cityWind"].feedData("&value="+wind);
+          }, 4);
+      }  
+  }
+  }).render();
+});
+
 
 // Create sunshine chart
+FusionCharts.ready(function() {
+  var myChart = new FusionCharts({
+    type: "hlineargauge",
+    renderAt: "sunshine",
+    id: "citySun",
+    width: "240",
+    height: "300",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Avg. Daily Minutes of Sunshine",
+        subcaption: "",
+        theme: "gammel",
+        showvalue: "0",
+        pointerbghovercolor: "#ffffff",
+        pointerbghoveralpha: "80",
+        pointerhoverradius: "12",
+        showborderonhover: "1",
+        pointerborderhovercolor: "#333333",
+        pointerborderhoverthickness: "2",
+        showtickmarks: "0",
+        numbersuffix: "min"
+      },
+      colorrange: {
+        color: [
+          {
+            minvalue: "0",
+            maxvalue: "480",
+            label: "Little Sunlight",
+            code: "#b9b9b3"
+          },
+          {
+            minvalue: "480",
+            maxvalue: "960",
+            label: "Moderate Sunlight",
+            code: "#fdfdfa"
+          },
+          {
+            minvalue: "960",
+            maxvalue: "1440",
+            label: "Lots of Sunlight",
+            code: "#ffff00"
+          }
+        ]
+      },
+      pointers: {
+        pointer: [
+          {
+            value: avg_sun,
+            tooltext: "$datavalue minutes of sunlight"
+          }
+        ]
+      }
+    };
+    "events" :{
+      "rendered" : function (evnt, argt) {
+          var chargeInterval = setInterval( function(){
+              var sun = avg_sun;
+              FusionCharts.items["citySun"].feedData("&value="+sun);
+          }, 4);
+      }  
+  }
+  }).render();
+});
